7902ad0c6fd9a4e6f3c916fffbd11da1d82e4d95
Nomena Faliana <kristianatenaizy@gmail.com>
2025-02-13 07:57:18 +0300

    add auth module

---
+++ back/src/modules/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { FirebaseService } from 'src/firebase/firebase.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly firebaseService: FirebaseService,
+  ) {}
+
+  @Post('register')
+  async registerUser(@Body() userData: { email: string; password: string }) {
+    const auth = this.firebaseService.getAuth();
+    const userRecord = await auth.createUser({
+      email: userData.email,
+      password: userData.password,
+    });
+    return {
+      message: 'User added successfuly',
+      user: { uid: userRecord.uid, email: userRecord.email },
+    };
+  }
+
+  @Post('signin')
+  async signIn(@Body('token') token: string) {
+    return this.authService.signInWithProvider(token);
+  }
+
+  @Post('signin/email')
+  async signInEmailPass(@Body('token') token: string) {
+    return this.authService.signInEmailPass(token);
+  }
+}
---
+++ back/src/modules/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { FirebaseService } from 'src/firebase/firebase.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly firebaseService: FirebaseService) {}
+
+  async signInWithProvider(token: string) {
+    const decodedToken = await this.firebaseService.verifyToken(token);
+
+    const user = {
+      uid: decodedToken.uid,
+      email: decodedToken.email,
+      name: decodedToken.name || '',
+      picture: decodedToken.picture || '',
+      provider: decodedToken.firebase.sign_in_provider,
+    };
+
+    return { message: 'Sign in successfuly', user };
+  }
+
+  async signInEmailPass(token: string) {
+    const decodedToken = await this.firebaseService.verifyToken(token);
+    const user = {
+      uid: decodedToken.uid,
+      email: decodedToken.email,
+      name: decodedToken.name || '',
+    };
+    return {
+      message: 'Sign in successfuly',
+      user,
+    };
+  }
+}
